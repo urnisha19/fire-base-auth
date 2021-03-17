9863895de35ac053f71e5a3c8130925be7b1e6d5
urnisha19 <urnisha09@gmail.com>
2021-03-17 07:47:51 +0600

    first commit

---
+++ src/firebase.config.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDc4D2DR69IjKYmV57YdVs3m-P0zpf6YH0",
+    authDomain: "fir-authpractice-01.firebaseapp.com",
+    projectId: "fir-authpractice-01",
+    storageBucket: "fir-authpractice-01.appspot.com",
+    messagingSenderId: "1055293337181",
+    appId: "1:1055293337181:web:f1d1209dac77e9246fa7b4"
+  };
+  export default firebaseConfig;
